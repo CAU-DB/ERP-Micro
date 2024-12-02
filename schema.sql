264f9bc8ec8846190f214645ef0d1b27220cb713
jeongho0715 <80153484+jeongho0715@users.noreply.github.com>
2024-12-02 20:08:19 +0900

    fix: execution order and attribute modifications

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE employees (
 
 CREATE TABLE employees_detail (
     employee_ID VARCHAR(7) PRIMARY KEY,
-    resident_number CHAR(13) NOT NULL,
+    resIDent_number VARCHAR(14) NOT NULL,
     address VARCHAR(255),
     cellphone VARCHAR(15),
     bank_account VARCHAR(20),
@@ -51,7 +51,12 @@ CREATE TABLE basic_salary (
     FOREIGN KEY (employee_ID) REFERENCES employees(employee_ID)
 );
 
-CREATE TABLE payment (
+CREATE TABLE payment_category (
+    pay_cat_ID VARCHAR(7) PRIMARY KEY,
+    category VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employee_payment (
     pay_ID VARCHAR(7) PRIMARY KEY,
     salary_ID VARCHAR(7) NOT NULL,
     pay_cat_ID VARCHAR(7) NOT NULL,
@@ -61,12 +66,6 @@ CREATE TABLE payment (
     FOREIGN KEY (salary_ID) REFERENCES basic_salary(salary_ID),
     FOREIGN KEY (pay_cat_ID) REFERENCES payment_category(pay_cat_ID),
     FOREIGN KEY (employee_ID) REFERENCES employees(employee_ID)
-);
-
-CREATE TABLE payment_category (
-    pay_cat_ID VARCHAR(7) PRIMARY KEY,
-    category VARCHAR(50) NOT NULL,
-    amount DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE attendance (
@@ -80,10 +79,17 @@ CREATE TABLE attendance (
 
 CREATE TABLE permission (
     dept_ID VARCHAR(7) PRIMARY KEY,
-    account_management TINYINT,
-    account_period_management TINYINT,
-    budget_management TINYINT,
+    account TINYINT,
+    account_period TINYINT,
+    budget TINYINT,
     FOREIGN KEY (dept_ID) REFERENCES departments(dept_ID)
+);
+
+CREATE TABLE project (
+    project_ID VARCHAR(7) PRIMARY KEY,
+    project_name VARCHAR(20) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
 );
 
 CREATE TABLE participation (
@@ -92,13 +98,6 @@ CREATE TABLE participation (
     PRIMARY KEY (dept_ID, project_ID),
     FOREIGN KEY (dept_ID) REFERENCES departments(dept_ID),
     FOREIGN KEY (project_ID) REFERENCES project(project_ID)
-);
-
-CREATE TABLE project (
-    project_ID VARCHAR(7) PRIMARY KEY,
-    project_name VARCHAR(20) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE
 );
 
 CREATE TABLE journal (
@@ -138,6 +137,14 @@ CREATE TABLE payment_card (
     FOREIGN KEY (bank_account_ID) REFERENCES bank_account(bank_account_ID)
 );
 
+CREATE TABLE account (
+    account_code VARCHAR(7) PRIMARY KEY,
+    account_name VARCHAR(45) NOT NULL,
+    debit_credit_indicator ENUM('Debit', 'Credit') NOT NULL,
+    account_type ENUM('Assets', 'Liabilities', 'Equity', 'Income', 'Expenses') NOT NULL,
+    is_active TINYINT NOT NULL
+);
+
 CREATE TABLE transaction (
     trans_ID VARCHAR(7) PRIMARY KEY,
     journal_ID VARCHAR(7) NOT NULL,
@@ -149,13 +156,13 @@ CREATE TABLE transaction (
     FOREIGN KEY (account_code) REFERENCES account(account_code)
 );
 
-CREATE TABLE use_account (
-    account_code VARCHAR(7) NOT NULL,
-    budget_ID VARCHAR(7) NOT NULL,
-    amount INT NOT NULL,
-    PRIMARY KEY (account_code, budget_ID),
-    FOREIGN KEY (account_code) REFERENCES account(account_code),
-    FOREIGN KEY (budget_ID) REFERENCES budget(budget_ID)
+CREATE TABLE account_period (
+    account_period_ID VARCHAR(7) PRIMARY KEY,
+    fiscal_year VARCHAR(4) NOT NULL,
+    period_name VARCHAR(45),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    is_closed TINYINT NOT NULL
 );
 
 CREATE TABLE budget (
@@ -168,67 +175,49 @@ CREATE TABLE budget (
     FOREIGN KEY (dept_ID) REFERENCES departments(dept_ID)
 );
 
-CREATE TABLE account_period (
-    account_period_ID VARCHAR(7) PRIMARY KEY,
-    fiscal_year VARCHAR(4) NOT NULL,
-    period_name VARCHAR(45),
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    is_closed TINYINT NOT NULL
-);
-
-CREATE TABLE account (
-    account_code VARCHAR(7) PRIMARY KEY,
-    account_name VARCHAR(45) NOT NULL,
-    debit_credit_indicator ENUM('Debit', 'Credit') NOT NULL,
-    account_type ENUM('Assets', 'Liabilities', 'Equity', 'Income', 'Expenses') NOT NULL,
-    is_active TINYINT NOT NULL
+CREATE TABLE use_account (
+    account_code VARCHAR(7) NOT NULL,
+    budget_ID VARCHAR(7) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY (account_code, budget_ID),
+    FOREIGN KEY (account_code) REFERENCES account(account_code),
+    FOREIGN KEY (budget_ID) REFERENCES budget(budget_ID)
 );
 
 CREATE TABLE customer (
-    customer_id VARCHAR(7) NOT NULL PRIMARY KEY,
+    customer_ID VARCHAR(7) NOT NULL PRIMARY KEY,
     customer_name VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     phone VARCHAR(20) NOT NULL,
     refund_account VARCHAR(255)
 );
 
-CREATE TABLE order_management (
-    order_id VARCHAR(7) NOT NULL PRIMARY KEY,
-    customer_id VARCHAR(7) NOT NULL,
+CREATE TABLE orders (
+    order_ID VARCHAR(7) NOT NULL PRIMARY KEY,
+    customer_ID VARCHAR(7) NOT NULL,
     order_price INT NOT NULL,
     order_date DATE NOT NULL,
     order_time TIME NOT NULL,
     order_status VARCHAR(50) NOT NULL,
     delivery_address VARCHAR(255) NOT NULL,
-    invoice_id VARCHAR(7),
-    journal_id VARCHAR(7),
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (journal_id) REFERENCES journal(journal_id)
+    invoice_ID VARCHAR(7),
+    journal_ID VARCHAR(7),
+    FOREIGN KEY (customer_ID) REFERENCES customer(customer_ID),
+    FOREIGN KEY (journal_ID) REFERENCES journal(journal_ID)
 );
 
-CREATE TABLE payment_management (
-    payment_id VARCHAR(7) NOT NULL PRIMARY KEY,
+CREATE TABLE payment (
+    payment_ID VARCHAR(7) NOT NULL PRIMARY KEY,
     payment_method VARCHAR(50) NOT NULL,
-    order_id VARCHAR(7) NOT NULL,
-    payment_price INT NOT NULL,
+    order_ID VARCHAR(7) NOT NULL,
     payment_date DATE NOT NULL,
     payment_time TIME NOT NULL,
     payment_status BOOLEAN NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES order_management(order_id)
+    FOREIGN KEY (order_ID) REFERENCES orders(order_ID)
 );
 
-CREATE TABLE order_product (
-    order_id VARCHAR(7) NOT NULL,
-    product_id VARCHAR(7) NOT NULL,
-    order_quantity INT NOT NULL,
-    PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES order_management(order_id),
-    FOREIGN KEY (product_id) REFERENCES product_management(product_id)
-);
-
-CREATE TABLE product_management (
-    product_id VARCHAR(7) NOT NULL PRIMARY KEY,
+CREATE TABLE product (
+    product_ID VARCHAR(7) NOT NULL PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     product_category VARCHAR(255) NOT NULL,
     price INT NOT NULL,
@@ -236,51 +225,61 @@ CREATE TABLE product_management (
     is_active BOOLEAN NOT NULL
 );
 
-CREATE TABLE inventory_management (
-    product_id VARCHAR(7) NOT NULL PRIMARY KEY,
-    quantity INT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product_management(product_id)
+CREATE TABLE order_product (
+    order_ID VARCHAR(7) NOT NULL,
+    product_ID VARCHAR(7) NOT NULL,
+    order_quantity INT NOT NULL,
+    PRIMARY KEY (order_ID, product_ID),
+    FOREIGN KEY (order_ID) REFERENCES orders(order_ID),
+    FOREIGN KEY (product_ID) REFERENCES product(product_ID)
 );
 
 CREATE TABLE warehouse (
-    warehouse_id VARCHAR(7) NOT NULL PRIMARY KEY,
-    product_id VARCHAR(7) NOT NULL,
-    warehouse_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES product_management(product_id)
+    warehouse_ID VARCHAR(7) PRIMARY KEY,
+    warehouse_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE inventory (
+    product_ID VARCHAR(7) NOT NULL,
+    warehouse_ID VARCHAR(7) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (product_ID, warehouse_ID),
+    FOREIGN KEY (product_ID) REFERENCES product(product_ID),
+    FOREIGN KEY (warehouse_ID) REFERENCES warehouse(warehouse_ID)
 );
 
 CREATE TABLE supplier (
-    supplier_id VARCHAR(7) NOT NULL PRIMARY KEY,
+    supplier_ID VARCHAR(7) NOT NULL PRIMARY KEY,
     supplier_name VARCHAR(255) NOT NULL,
     supplier_phone VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE supply_management (
-    product_id VARCHAR(7) NOT NULL,
-    supplier_id VARCHAR(7) NOT NULL,
+CREATE TABLE supply (
+    product_ID VARCHAR(7) NOT NULL,
+    supplier_ID VARCHAR(7) NOT NULL,
     cost INT NOT NULL,
-    journal_id VARCHAR(7),  -- 연결된 journal_id 추가
-    PRIMARY KEY (product_id, supplier_id),
-    FOREIGN KEY (product_id) REFERENCES product_management(product_id),
-    FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id),
-    FOREIGN KEY (journal_id) REFERENCES journal(journal_id)  -- journal 테이블과 연결
-);
-
-CREATE TABLE production_management (
-    production_id VARCHAR(7) NOT NULL PRIMARY KEY,
-    product_id VARCHAR(7) NOT NULL,
-    production_quantity INT NOT NULL,
-    production_date DATE NOT NULL,
-    production_time TIME NOT NULL,
-    factory_id VARCHAR(7) NOT NULL,
-    journal_id VARCHAR(7),  -- 연결된 journal_id 추가
-    FOREIGN KEY (product_id) REFERENCES product_management(product_id),
-    FOREIGN KEY (factory_id) REFERENCES factory(factory_id),
-    FOREIGN KEY (journal_id) REFERENCES journal(journal_id)  -- journal 테이블과 연결
+    journal_ID VARCHAR(7),
+    PRIMARY KEY (product_ID, supplier_ID),
+    FOREIGN KEY (product_ID) REFERENCES product(product_ID),
+    FOREIGN KEY (supplier_ID) REFERENCES supplier(supplier_ID),
+    FOREIGN KEY (journal_ID) REFERENCES journal(journal_ID)
 );
 
 CREATE TABLE factory (
-    factory_id VARCHAR(7) NOT NULL PRIMARY KEY,
+    factory_ID VARCHAR(7) NOT NULL PRIMARY KEY,
     factory_name VARCHAR(255) NOT NULL,
     factory_phone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE production (
+    production_ID VARCHAR(7) NOT NULL PRIMARY KEY,
+    product_ID VARCHAR(7) NOT NULL,
+    production_quantity INT NOT NULL,
+    production_date DATE NOT NULL,
+    production_time TIME NOT NULL,
+    factory_ID VARCHAR(7) NOT NULL,
+    journal_ID VARCHAR(7),
+    FOREIGN KEY (product_ID) REFERENCES product(product_ID),
+    FOREIGN KEY (factory_ID) REFERENCES factory(factory_ID),
+    FOREIGN KEY (journal_ID) REFERENCES journal(journal_ID)
 );
